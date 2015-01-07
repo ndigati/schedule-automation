eb669795f436ed09a656c73d93bd870dc60f2172
Nicholas Digati <ndigati@gmail.com>
2015-01-06 23:45:21 -0800

    Fixed group .group being used on the wrong part of the line, also fixed some formating errors.

--- schedule_automation/schedule.py
+++ schedule_automation/schedule.py
@@ -4,16 +4,18 @@ import re
 from bs4 import BeautifulSoup
 
 
-# global list of days of the week so it is available to all funcitons
+# global list of days of the week so it is available to all functions
 WORK_DAYS = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    ]
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday",
+]
+
 
 def get_login_info(filename):
-    '''
-    This method is used to extract login info from the config (.ini) file in the
-    same directory as the script file. Stores this login info in a dictionary
-    and returns it to be used to login to the website.
+    """
+    This method is used to extract login info from the config (.ini) file in
+    the same directory as the script file. Stores this login info in a
+    dictionary and returns it to be used to login to the website.
 
     No error handling yet, needs to be done soon.
 
@@ -26,7 +28,7 @@ def get_login_info(filename):
     Return:
         logins = dictionary containing login info
         (key: username, value: password)
-    '''
+    """
     logins = {}
     config = configparser.ConfigParser()
     config.read(filename)
@@ -34,29 +36,32 @@ def get_login_info(filename):
     logins[config['CREW_LOGIN']['username']] = config['CREW_LOGIN']['password']
     return logins
 
+
 def http_fetch():
-    '''
+    """
     This method is used to login to the whentowork website and get the HTML
     containing the current week schedule.
-    '''
-    logins = get_login_info('login.ini')
+    """
+    logins = get_login_info(
+        'C:/Users/Nick/PycharmProjects/schedule-automation/login.ini')
     # Get the correct login from the returned dictionary
     # Need to add handling for when only one login is present in config.
-    crewLogin = ()
-    usherLogin = ()
+    crew_login = ()
+    usher_login = ()
     for key in logins:
         if 'crew' in key:
-            crewLogin = (key, logins[key])
+            crew_login = (key, logins[key])
         else:
-            usherLogin = (key, logins[key])
+            usher_login = (key, logins[key])
 
     # Set up the payload to pass to requests session
-    payload = {"NAS_id": 76003,
-            "name": "signin",
-            "UserId1": crewLogin[0] ,
-            "Password1": crewLogin[1],
-            "Submit1": "Please Wait..."
-            }
+    payload = {
+        "NAS_id": 76003,
+        "name": "signin",
+        "UserId1": crew_login[0],
+        "Password1": crew_login[1],
+        "Submit1": "Please Wait..."
+    }
 
     # Using a session so it can keep cookies to not have to log in again when
     # navigating to a new page.
@@ -69,18 +74,19 @@ def http_fetch():
     # Since it is dynamically generated every login, use this to get those last
     # 13 characters. Can then append this SID to the end of any W2W pages.
     sid = r.url[index+1:]
-    r = s.get('https://www3.whentowork.com/cgi-bin/w2wC.dll/empschedule?SID={0}'
-            .format(sid))
+    r = s.get('https://www3.whentowork.com/cgi-bin/w2wC.dll/empschedule?SID'
+              '={0}'.format(sid))
     return r.text
     #parse_html(r.text)
 
+
 def parse_html(html):
-    '''
+    """
     Method to get the HTML containing the currently scheduled work days.
 
     Parameters:
         html = string of HTML containing work schedule.
-    '''
+    """
     soup = BeautifulSoup(html, 'html.parser')
     # Schedule is in an html table tag with the class bd.
     # Actual shift information is within a script tag under that table tag
@@ -96,24 +102,25 @@ def parse_html(html):
     return shifts
     #working_shifts = extract_shifts(shifts)
 
+
 def extract_shifts(shifts):
-    '''
+    """
     Method to extract and return the exact day, time, and shift info from the
     HTML containg the information.
 
     Parameters:
         shifts = list of HTML strings containing shift information
         days = list of days of the week to create final dict
-    '''
+    """
     pattern = re.compile(r"\"[0-9]+[a|p]m(.)*\"")
     working_shifts = {}
     for day in WORK_DAYS:
         working_shifts[day] = []
         for shift in shifts:
             if day in shift:
-               shift_info = re.search(pattern, shift)
-               if shift_info:
-                   working_shifts[day].append(shift_info).group(0)
+                shift_info = re.search(pattern, shift)
+                if shift_info:
+                    working_shifts[day].append(shift_info.group(0))
     return working_shifts
 
 
